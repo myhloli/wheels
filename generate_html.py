79f81aedadcee2a05487d6c4ab736f03dcd1e1b2
myhloli <moe@myhloli.com>
2024-07-13 00:24:23 +0800

    Add GitHub Actions workflow for deploying generated HTML to GitHub Pages

---
+++ generate_html.py
@@ -0,0 +1,36 @@
+from jinja2 import Template
+import os
+
+# 定义HTML模板
+template_str = """
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="pypi:repository-version" content="1.1">
+</head>
+<body>
+{% for whl_file in whl_files %}
+<a href="{{ base_url }}{{ whl_file }}">{{ whl_file }}</a><br>
+{% endfor %}
+</body>
+</html>
+"""
+
+# 创建Jinja2模板对象
+template = Template(template_str)
+
+# 目录路径，这里假设是当前目录下的whl文件夹
+directory_path = 'assets/whl'
+
+# 获取目录下所有的.whl文件
+whl_files = [f for f in os.listdir(directory_path) if f.endswith('.whl')]
+
+# 基础URL，这里假设是你的GitHub Pages的URL
+base_url = 'https://mylhloli.github.io/wheels/'
+
+# 渲染HTML模板
+html_content = template.render(whl_files=whl_files, base_url=base_url)
+
+# 写入HTML文件
+with open('index.html', 'w') as file:
+    file.write(html_content)
