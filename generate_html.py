ed4168955c24ea9a5d31b8bc7cd9477ac295aa9a
myhloli <moe@myhloli.com>
2024-07-30 19:47:23 +0000

    deploy: bf15d714d70d925db629e4027251c616c4e1fb8e

--- generate_html.py
+++ generate_html.py
@@ -65,7 +65,10 @@ packages = next(os.walk(directory_path))[1]
 # base_url = 'https://gitee.com/myhloli/wheels/raw/main/assets/whl' # 外站引用会403
 
 # 腾讯云cos
-base_url = 'https://wheels-1251341229.cos.ap-shanghai.myqcloud.com/assets/whl'
+# base_url = 'https://wheels-1251341229.cos.ap-shanghai.myqcloud.com/assets/whl'
+
+# 腾讯云cos cdn回源
+base_url = "https://wheels-1251341229.cos.ap-shanghai.myqcloud.com/myhloli/wheels/raw/main/assets/whl"
 
 index_base_url = 'https://myhloli.github.io/wheels/'
 
